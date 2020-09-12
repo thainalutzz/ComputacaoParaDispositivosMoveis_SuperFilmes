f7bda06c902ffc4712ac9701ac577529b1ad98bd
thainalutzz <thainaslutz@gmail.com>
2020-09-12 15:23:35 -0300

    Super Filmes

---
+++ SuperFilmes/app/src/main/java/com/example/superfilmes/Generos.java
@@ -0,0 +1,14 @@
+package com.example.superfilmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Generos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generos);
+    }
+}
